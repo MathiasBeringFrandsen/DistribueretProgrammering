c18e11771d0afed26028d8ee1914c1ee48746de2
Mathias <tidspe@gmail.com>
2023-01-09 21:55:47 +0100

    wef

--- Eksamensprogrammering/spm4/app.js
+++ Eksamensprogrammering/spm4/app.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const session = require('express-session');
@@ -10,11 +11,20 @@ app.use(express.json());
 app.use(session({secret: 'hemmelig', saveUninitialized: true, resave: true}));
 
 app.get('/', function (request, response) {
-response.render("index");
+    if (request.session.personer === undefined){
+        request.session.personer = [];
+    }
+    response.render("index", {personer : request.session.personer});
 });
 
 app.post('/', function (request, response) {
-
+    if (request.session.personer === undefined){
+        request.session.personer = [];
+    }
+    request.session.personer.push({name : request.body.navn, adresse: request.body.adresse});
+    console.log(request.session.personer);
+    response.redirect('/');
+    response.status(201).end();
 });
 
 app.listen(8080);
